849eb2839acec602ef29fa57b058470797247020
ekanandaputra <52701387+ekanandaputra@users.noreply.github.com>
2021-10-31 19:42:30 +0700

    Install Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+import Vue from 'vue'
+import About from './components/AboutComponent.vue'
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        "about-us": About
+    }
+});
